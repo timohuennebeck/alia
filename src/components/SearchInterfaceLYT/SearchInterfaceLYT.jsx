5cd682aa548716bf82731140db0456e3a64003cc
timohuennebeck <hello@timohuennebeck.com>
2022-11-18 21:48:29 +0100

    add search interface desktop design

--- src/components/SearchInterfaceLYT/SearchInterfaceLYT.jsx
+++ src/components/SearchInterfaceLYT/SearchInterfaceLYT.jsx
@@ -2,18 +2,43 @@ import "./SearchInterfaceLYT.scss";
 
 import Header from "../Header/Header";
 import { Outlet } from "react-router-dom";
+import FriendsPage from "../../pages/FriendsPage/FriendsPage";
+import FavoriteHubsPage from "../../pages/FavoriteHubsPage/FavoriteHubsPage";
+import UpcomingMeetingsPage from "../../pages/UpcomingMeetingsPage/UpcomingMeetingsPage";
+import UpcomingEventsPage from "../../pages/UpcomingEventsPage/UpcomingEventsPage";
+import TrendingForYouPage from "../../pages/TrendingForYouPage/TrendingForYouPage";
 import searchImg from "../../assets/icons/search.svg";
 
 export default function SearchInterfaceLYT() {
     return (
         <div className="search">
-            <Header />
-            <div className="search__input">
-                <img className="search__input-img" src={searchImg} alt="" />
-                <input className="search__input-input" placeholder="Search..." />
+            <div className="search__mobile">
+                <Header />
+                <div className="search__mobile-explore">
+                    <img className="search__mobile-explore-img" src={searchImg} alt="" />
+                    <input className="search__mobile-explore-input" placeholder="Search..." />
+                </div>
             </div>
-            <div>
-                <Outlet />
+            <div className="search__desktop">
+                <div className="search__desktop-left">
+                    <div className="search__desktop-left-explore">
+                        <input
+                            className="search__desktop-left-explore-input"
+                            placeholder="# Explore"
+                        />
+                    </div>
+                    <FriendsPage />
+                    <FavoriteHubsPage />
+                </div>
+                <div className="search__desktop-main">
+                    <Outlet />
+                </div>
+                <div className="search__desktop-right">
+                    <Header />
+                    <UpcomingMeetingsPage />
+                    <UpcomingEventsPage />
+                    <TrendingForYouPage />
+                </div>
             </div>
         </div>
     );
