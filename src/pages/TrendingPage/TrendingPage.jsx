aa52f966a347d9da123cf1632fd22e0e3ff019d9
timohuennebeck <hello@timohuennebeck.com>
2022-11-19 07:47:58 +0100

    remove unused images

--- src/pages/TrendingPage/TrendingPage.jsx
+++ src/pages/TrendingPage/TrendingPage.jsx
@@ -1,20 +1,11 @@
 import "./TrendingPage.scss";
 
 // icons
-import paperImg from "../../assets/icons/paperclip.svg";
-import imagesImg from "../../assets/icons/image-gallery.svg";
-import liveImg from "../../assets/icons/cast.svg";
-import clockImg from "../../assets/icons/clock.svg";
-import usersImg from "../../assets/icons/users.svg";
 import micImg from "../../assets/icons/microphone.svg";
 import bellImg from "../../assets/icons/bell-slash.svg";
 import moreImg from "../../assets/icons/more-h.svg";
 
-// images
-import profileImg from "../../assets/images/melanie-perkins.jpg";
-
 // components
-import InputField from "../../components/InputField/InputField";
 import NewComment from "../../components/NewComment/NewComment";
 
 export default function TrendingPage() {
