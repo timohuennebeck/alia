df82fb3cd26a5de676ee4a88c2fd2a3a58c5a7cb
timohuennebeck <hello@timohuennebeck.com>
2023-01-10 15:12:26 +0100

    change name

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -11,26 +11,33 @@ export default function LandingPage() {
 
     return (
         <div className="landing">
-            <div className="landing__header">
-                <div className="landing__header-buttons">
+            <div className="landing__ctn">
+                <div className="landing__ctn-access">
+                    <p>Don't have an account? Log in using a sample account:</p>
+                    <p>Email: beta@fuse.com and PW: !8n3MapQGt?7fYyQ</p>
+                </div>
+                <div className="landing__ctn-indv">
+                    <p className="landing__ctn-indv-version">Alpha Version 1.0</p>
+                    <img className="landing__ctn-indv-logo" src={aliaLogo} alt="" />
+                    <p className="landing__ctn-indv-welcome">Welcome to Fuse!</p>
+                    <p className="landing__ctn-indv-account">
+                        Log in with a Google account or sign up using email to continue.
+                    </p>
+                </div>
+                <div className="landing__ctn-login">
                     <button
                         onClick={() => loginWithRedirect({ screen_hint: "signup" })}
-                        className="landing__header-buttons-indv"
+                        className="landing__ctn-login-buttons"
                     >
                         Sign Up
                     </button>
                     <button
                         onClick={() => loginWithRedirect()}
-                        className="landing__header-buttons-indv"
+                        className="landing__ctn-login-buttons"
                     >
                         Log In
                     </button>
                 </div>
-                <p className="landing__header-version">Alpha Version 1.0</p>
-            </div>
-            <div className="landing__img">
-                <p className="landing__img-version">Alpha Version 1.0</p>
-                <img className="landing__img-mobile" src={aliaLogo} alt="" />
             </div>
         </div>
     );
